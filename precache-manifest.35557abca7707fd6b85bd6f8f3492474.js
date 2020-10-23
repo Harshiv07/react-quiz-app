bbf643b08275ccf5537096f6839cbb67ae86825e
Harshiv07 <pharshiv10@gmail.com>
2020-10-23 10:57:21 +0530

    Updates

---
+++ precache-manifest.35557abca7707fd6b85bd6f8f3492474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8652f7c83c22fac82ce824e023225285",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "a06f589f6802a4c0192b",
+    "url": "/react-quiz-app/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "44fa082fa55eb5f1a718",
+    "url": "/react-quiz-app/static/js/2.6fcf88c1.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-quiz-app/static/js/2.6fcf88c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06f589f6802a4c0192b",
+    "url": "/react-quiz-app/static/js/main.7bd763e5.chunk.js"
+  },
+  {
+    "revision": "4cb17b25e4d85c130919",
+    "url": "/react-quiz-app/static/js/runtime-main.0678ee4d.js"
+  }
+]);
